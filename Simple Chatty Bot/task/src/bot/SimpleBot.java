92a44e7b082e5bf0bd3567ced7fb4bd1bf1f97ae
Laya1280 <layaml1280@gmail.com>
2020-09-03 19:32:18 +0530

    Second Stage

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,8 +1,18 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Aid");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
         System.out.println("I was created in 2020");
+        System.out.println("Please, remind me your name.");
+
+        String userName = scanner.next();
+
+        System.out.println("What a great name you have, {}!\n".format(userName));
+
     }
 }
